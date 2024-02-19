e7fc97ecad0662882d57a556d5f3a206b06755c6
João Victor da Silva <jv782063@gmail.com>
2024-02-19 11:03:13 -0300

    creates script to generate files to simulate file uploads

---
+++ cmd/generator/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+  for i := 0; i < 1000; i++ {
+    file, err := os.Create(fmt.Sprintf("./tmp/file_%d.txt", i))
+    if err != nil {
+      log.Panic(err)
+    }
+    defer file.Close()
+
+    file.WriteString("Hello, World!")
+  }
+}
